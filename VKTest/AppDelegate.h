3c3f1cf3b39876df2e599948e4c60073ec2b4ff7
–Ш–≥–Њ—А—М –Т–µ–і–µ–љ–µ–µ–≤ <igorv@mac-mini-igor.local>
2015-09-28 17:06:18 +0300

    Initial Commit

---
+++ VKTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VKTest
+//
+//  Created by Игорь Веденеев on 28.09.15.
+//  Copyright © 2015 Игорь Веденеев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VKTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VKTest
+//
+//  Created by Игорь Веденеев on 28.09.15.
+//  Copyright © 2015 Игорь Веденеев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
